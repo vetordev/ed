f5cac2d19b0d19d66ca3617618e0bcbd35b520b1
vetor d'oro <vitor13m2003@gmail.com>
2022-08-30 10:10:32 -0300

    new: verify expression

--- go/stack/ex1-balancing.go
+++ go/stack/ex1-balancing.go
@@ -1,7 +1,33 @@
 package stack
 
-// To evaluate if a expression is valid
+// To evaluate if an expression is balanced
 // expr? {[()]}
-func evalexpr(expr string) {
 
+func IsBalancedExpr(expr string) bool {
+	stack := NewStack[int32]()
+
+	for _, char := range expr {
+		switch char {
+		case '{':
+			stack.push('}')
+			break
+		case '[':
+			stack.push(']')
+			break
+		case '(':
+			stack.push(')')
+			break
+		default:
+			if item, err := stack.pop(); err != nil || item != char {
+				return false
+			}
+
+		}
+	}
+
+	if !stack.empty() {
+		return false
+	}
+
+	return true
 }
