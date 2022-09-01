1edfabfe0cf5895fcddacdcc8259827727cc0a91
vetor d'oro <vitor13m2003@gmail.com>
2022-08-31 23:45:28 -0300

    test: stack

--- go/stack/stack_test.go
+++ go/stack/stack_test.go
@@ -5,9 +5,56 @@ import (
 )
 
 func TestBalanced(t *testing.T) {
-	balanced := IsBalancedExpr("{([])}")
+	expr := "{([])}"
+	balanced := IsBalancedExpr(expr)
 
-	if balanced != true {
-		t.Fatalf(`Balanced("{([])}") = %q`, "a")
+	want := true
+
+	if balanced != want {
+		t.Fatalf(`Balanced(%q) = %v, want match for %v`, expr, balanced, want)
+	}
+}
+
+func TestNotBalanced(t *testing.T) {
+	expr := "{([})"
+	balanced := IsBalancedExpr(expr)
+
+	want := false
+
+	if balanced != want {
+		t.Fatalf(`Balanced(%q) = %v, want match for %v`, expr, balanced, want)
+	}
+}
+
+func TestPostFix(t *testing.T) {
+	mathExpr := "((2*3)+(8/4))"
+	postfix := PostfixMathExpr(mathExpr)
+
+	want := "23*84/+"
+
+	if postfix != want {
+		t.Fatalf(`PostfixMathExpr(%q) = %q, want match for %q`, mathExpr, postfix, want)
+	}
+}
+
+func TestSumPostFixed(t *testing.T) {
+	postFixed := "23*84/+"
+	sum := SumPostFixedExpr(postFixed)
+
+	want := 8.0
+
+	if sum != want {
+		t.Fatalf(`SumPostFixedExpr(%q) = %g, want match for %g`, postFixed, sum, want)
+	}
+}
+
+func TestPostFixImproved(t *testing.T) {
+	mathExpr := "245*342+82/43"
+	postfix := PostfixMathExpr(mathExpr)
+
+	want := "245 342*82 43/+"
+
+	if postfix != want {
+		t.Fatalf(`PostfixMathExpr(%q) = %q, want match for %q`, mathExpr, postfix, want)
 	}
 }
