3ca807a5df18387c2cce988086efdca1a9419bff
vetor d'oro <vitor13m2003@gmail.com>
2022-09-03 17:23:16 -0300

    new: make default func

--- go/stack/ex3-maze.go
+++ go/stack/ex3-maze.go
@@ -1,9 +1,21 @@
 package stack
 
+import "os"
+
+const (
+	Free = 0
+	Wall = 1
+)
+
 type Maze struct {
-	squares [][]int
-	start   Coordinates
+	squares [][]Square
+	begin   Coordinates
 	end     Coordinates
+}
+
+type Square struct {
+	state   int
+	crossed bool
 }
 
 type Coordinates struct {
@@ -11,7 +23,19 @@ type Coordinates struct {
 	Y int
 }
 
-func (m *Maze) PrintMaze(player Coordinates) string {
+func (m *Maze) Begin() Coordinates {
+	return m.begin
+}
+
+func (m *Maze) AtTheBeginning(coordinates Coordinates) bool {
+	return m.begin == coordinates
+}
+
+func (m *Maze) AtTheEnd(coordinates Coordinates) bool {
+	return m.end == coordinates
+}
+
+func (m *Maze) PrintMaze(file string, player Coordinates) {
 
 	data := ""
 	for x, row := range m.squares {
@@ -21,9 +45,9 @@ func (m *Maze) PrintMaze(player Coordinates) string {
 			coordinates := Coordinates{x, y}
 			if coordinates == player {
 				ch = "👾"
-			} else if square == 1 {
+			} else if square.state == Wall {
 				ch = "🧱"
-			} else if m.isStart(coordinates) || m.isEnd(coordinates) {
+			} else if m.AtTheBeginning(coordinates) || m.AtTheEnd(coordinates) {
 				ch = "🚪"
 			}
 
@@ -32,27 +56,31 @@ func (m *Maze) PrintMaze(player Coordinates) string {
 		data += "\n"
 	}
 
-	return data
-}
-
-func (m *Maze) isStart(coordinates Coordinates) bool {
-	return m.start == coordinates
-}
-
-func (m *Maze) isEnd(coordinates Coordinates) bool {
-	return m.end == coordinates
+	os.WriteFile(file, []byte(data), 0600)
 }
 
 func CreateMaze() *Maze {
+
+	defaultMaze := [][]int{
+		{Wall, Wall, Wall, Wall, Wall, Wall},
+		{Free, Free, Wall, Wall, Wall, Wall},
+		{Wall, Free, Free, Free, Wall, Wall},
+		{Wall, Free, Wall, Free, Wall, Wall},
+		{Wall, Free, Wall, Free, Free, Free},
+		{Wall, Wall, Wall, Wall, Wall, Wall},
+	}
+
+	squares := make([][]Square, len(defaultMaze))
+
+	for x, row := range defaultMaze {
+		squares[x] = make([]Square, len(defaultMaze[0]))
+		for y, state := range row {
+			squares[x][y] = Square{state, false}
+		}
+	}
+
 	return &Maze{
-		[][]int{
-			{1, 1, 1, 1, 1, 1},
-			{0, 0, 1, 1, 1, 1},
-			{1, 0, 0, 0, 1, 1},
-			{1, 0, 1, 0, 1, 1},
-			{1, 0, 1, 0, 0, 0},
-			{1, 1, 1, 1, 1, 1},
-		},
+		squares,
 		Coordinates{1, 0},
 		Coordinates{4, 5},
 	}
